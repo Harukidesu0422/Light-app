4db05be080f020076f846318c496e40570b23dc5
haruki <ndcan2008@gmail.com>
2024-06-16 10:13:24 +0700

    Initial Commit

---
+++ Light app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light app
+//
+//  Created by Nguyen Haruki on 16/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
